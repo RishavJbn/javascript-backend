dd89fed062aef604a2aa344396534b2c637c14bc
Rishavsingh-jbn <rishavsinghjbn@gmail.com>
2025-08-19 13:25:06 +0530

    connect db

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose, { connect } from "mongoose";
+import {DB_NAME} from "../constants.js";
+
+
+const connectDB = async () => {
+    try {
+        const dbInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log("MONGODB Connected ----> ", dbInstance.connection.host);
+    } catch (error) {
+      console.log("MONGODB Failed ----> ", error)
+      process.exit(1);  
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import "dotenv/config"; // this automatically runs dotenv.config()
+import express from "express";
+import connectDB from "./db/index.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+connectDB()
+.then(()=> {
+  app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}  \n Jai Bhole Nath`);
+  });
+})
+.catch((error) => {
+    console.log("MONGODB CONNECTION ERROR:", error);
+});
+ 
